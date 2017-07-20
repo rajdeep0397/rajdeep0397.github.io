727f26ca29c26b68139eb92a6297d812cbadd43f
Rajdeep Sarma <rajdeep0397@gmail.com>
2017-07-20 11:15:17 -0700

    on and off functions added

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+var state = 0;
+
 function calculate(){
 	
 document.calc.output.value=eval(document.calc.output.value);	
@@ -5,11 +7,27 @@ document.calc.output.value=eval(document.calc.output.value);
 	
 	
 }
-function erase(){
-	
-	document.calc.output.value=' ';
+function erase(){	
+	document.calc.output.value=0;
 }
 
 function entries(s){
-	document.calc.output.value+=s;	
+	if(state==1){
+		if(document.calc.output.value==0){
+			document.calc.output.value="";
+		}
+		document.calc.output.value+=s;
+	}
+}
+
+function on(){
+	if(state==0){
+		document.calc.output.value=0;
+		state = 1;
+	}
+}
+
+function off(){
+	erase();
+	state = 0;
 }
